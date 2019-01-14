d87bc89679c27198dfa355ec750fc272ab4bc5dd
Leandre <leandre.blanchard@epitech.eu>
2019-01-14 22:34:41 +0100

    clang format

--- src/server.c
+++ src/server.c
@@ -20,7 +20,8 @@ int lsocket_server(lsocket_t *lsocket, uint16_t port, int backlog)
   lsocket->saddr.sin_port = htons(port);
   if (bind(lsocket->fd, p, sizeof(lsocket->saddr)) == -1)
     return (-1);
-  if ((port == 0 && getsockname(lsocket->fd, p, &len) == -1) || listen(lsocket->fd, backlog) == -1)
+  if ((port == 0 && getsockname(lsocket->fd, p, &len) == -1)
+      || listen(lsocket->fd, backlog) == -1)
     return (-1);
   lsocket->port = htons(lsocket->saddr.sin_port);
   return (0);
