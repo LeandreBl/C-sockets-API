c775ed8cfc58f010ca537664cc2457036620d2e0
Leandre <leandre.blanchard@epitech.eu>
2019-01-31 00:39:18 +0100

    using random ports for tests

--- tests/socket_tests.c
+++ tests/socket_tests.c
@@ -1,13 +1,17 @@
 #include <criterion/criterion.h>
+#include <time.h>
 #include "lsocket.h"
 
 Test(server, localhost)
 {
   lsocket_t server;
   lsocket_t client;
+  char *cmd;
 
-  cr_assert(lsocket_server(&server, 1234, 5) == 0);
-  system("sleep 1 && nc 127.0.0.1 1234&");
+  cr_assert(lsocket_server(&server, 0, 5) == 0);
+  cr_asprintf(&cmd, "sleep 1 && nc 127.0.0.1 %d&", server.port);
+  system(cmd);
+  free(cmd);
   cr_assert(lsocket_accept(&server, &client) == 0);
   cr_assert(lsocket_shutdown(&server) == 0);
   cr_assert(lsocket_destroy(&server) == 0);
@@ -16,11 +20,17 @@ Test(server, localhost)
 Test(connect, localhost)
 {
   lsocket_t client;
-  if (system("nc -l 1235&") == -1) {
-    cr_log_error("Could not start netcat on port 1235");
+  char *cmd;
+  uint16_t port = 2000 + rand() % 20000;
+
+  srand(time(NULL));
+  cr_asprintf(&cmd, "nc -l %u&", port);
+  if (system(cmd) == -1) {
+    cr_log_error("Could not start netcat on port %u\n", port);
     return;
   }
-  cr_assert(lsocket_connect(&client, "127.0.0.1", 1235) == 0);
+  free(cmd);
+  cr_assert(lsocket_connect(&client, "127.0.0.1", port) == 0);
   cr_assert(lsocket_shutdown(&client) == 0);
   cr_assert(lsocket_destroy(&client) == 0);
 }
@@ -28,11 +38,17 @@ Test(connect, localhost)
 Test(connect32, localhost)
 {
   lsocket_t client;
-  if (system("nc -l 1236&") == -1) {
-    cr_log_error("Could not start netcat on port 1236");
+  char *cmd;
+  uint16_t port = 2000 + rand() % 20000;
+
+  srand(time(NULL));
+  cr_asprintf(&cmd, "nc -l %u&", port);
+  if (system(cmd) == -1) {
+    cr_log_error("Could not start netcat on port %u\n", port);
     return;
   }
-  cr_assert(lsocket_connect32(&client, 0, 1236) == 0);
+  free(cmd);
+  cr_assert(lsocket_connect32(&client, 0, port) == 0);
   cr_assert(lsocket_shutdown(&client) == 0);
   cr_assert(lsocket_destroy(&client) == 0);
 }
